c80678e59ba244a5857ebc33067943b5e440a4c3
jdy8739 <jdy8739@naver.com>
2024-02-08 23:37:59 +0900

    11650 풀이 커밋

---
+++ BAEKJOON/SORT/11650.js
@@ -0,0 +1,14 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+const arr = input
+  .slice(1, input.length - 1)
+  .map((el) => el.split(" ").map(Number));
+
+arr.sort((a, b) => (a[0] === b[0] ? a[1] - b[1] : a[0] - b[0]));
+
+let answer = "";
+
+arr.forEach((el) => (answer += el.join(" ") + "\n"));
+
+console.log(answer);
